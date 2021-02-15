ba7fe9ff9de1216c91f623f58af5a3de2ee116cf
Paul <plmacovei@gmail.com>
2021-02-14 16:37:01 -0800

    Update git-scan.sh

--- git-scan.sh
+++ git-scan.sh
@@ -1,4 +1,11 @@
-# Use git-secrets binary in order to protect any new git repos from accidental secret pushes
-git clone https://github.com/awslabs/git-secrets.git ~/
-cd git-secrets
-sudo make install
+mkdir ~/bootstrap/
+
+# Git-secrets
+git clone https://github.com/awslabs/git-secrets.git ~/bootstrap/git-secrets
+sudo make install -C ~/bootstrap/git-secrets
+rm -r ~/bootstrap/git-secrets
+
+# BFG Repo Cleaner
+wget https://repo1.maven.org/maven2/com/madgag/bfg/1.13.2/bfg-1.13.2.jar -O ~/bootstrap/bfg.jar
+echo "alias bfg='java -jar ~/bootstrap/bfg.jar'" >> ~/.bashrc
+source ~/.bashrc
