35306c42d2e9807f3cde8f9715e5d36c023e416c
Paul <5972492+paulyy-y@users.noreply.github.com>
2025-05-18 18:35:50 -0700

    More resilient

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,18 +19,33 @@ handle_error() {
 trap 'handle_error $LINENO' ERR
 
 mkdir -p ~/bootstrap/
+sudo apt install make
 
 # Git-secrets
-git clone https://github.com/awslabs/git-secrets.git ~/bootstrap/git-secrets
-sudo make install -C ~/bootstrap/git-secrets
-rm -r ~/bootstrap/git-secrets
+if [ ! -d "/usr/local/bin/git-secrets" ]; then
+    echo "Installing git-secrets..."
+    # Clean up any previous failed install attempts
+    rm -rf ~/bootstrap/git-secrets
+    git clone https://github.com/awslabs/git-secrets.git ~/bootstrap/git-secrets
+    sudo make install -C ~/bootstrap/git-secrets
+    rm -rf ~/bootstrap/git-secrets
+else
+    echo "git-secrets is already installed"
+fi
 
 # BFG Repo Cleaner
-wget https://repo1.maven.org/maven2/com/madgag/bfg/1.13.2/bfg-1.13.2.jar -O ~/bootstrap/bfg.jar
-if [ -f ~/.zshrc ]; then
-    echo "alias bfg='java -jar ~/bootstrap/bfg.jar'" >> ~/.zshrc
-else
+if [ ! -f ~/bootstrap/bfg.jar ]; then
+    echo "Downloading BFG Repo Cleaner..."
+    wget https://repo1.maven.org/maven2/com/madgag/bfg/1.13.2/bfg-1.13.2.jar -O ~/bootstrap/bfg.jar
+fi
+
+# Add BFG alias if not already present
+if ! grep -q "alias bfg='java -jar ~/bootstrap/bfg.jar'" ~/.bashrc; then
+    echo "Adding BFG alias to .bashrc..."
     echo "alias bfg='java -jar ~/bootstrap/bfg.jar'" >> ~/.bashrc
+    source ~/.bashrc
+else
+    echo "BFG alias already exists in .bashrc"
 fi
 
 sudo apt install git-filter-repo
